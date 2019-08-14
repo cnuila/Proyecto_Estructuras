c2781633aacab757b356aef11793a5b2b2c3ab88
Carlos Nuila <cnuia14@icloud.com>
2019-08-14 03:08:12 -0600

    Actualizando

--- ArrayStack.py
+++ ArrayStack.py
@@ -2,19 +2,44 @@ from Pila import Pila
 
 class ArrayStack(Pila):
 
+    #constructor
     def __init__(self):
         self.size = 0
-        self.capacity = 1024
         self.arreglo = []
     
+    #elimina todos los elementos de la pila
     def anula(self):
         for i in range(self.size):
             if self.arreglo[i] != None:
-                self.arreglo[i] = None       
-        size = 0
+                self.arreglo[i] = None    
+        self.size = 0 #reinicia el size
     
-    def resize(self):
-        newCapacity = self.capacity*2
+    #agrega un elemento a la pila 
+    def empuja(self,data):
+        self.arreglo[size] = data #agrega el dato enviado al final del arreglo
+        self.size+1
+    
+    #saca el elemento en el tope de la pila
+    def saca(self):
+        if self.size == 0:
+            return None
+        else:
+            datoTemp = self.arreglo[self.size-1] #se guarda temporalmente para mostrarlo
+            self.arreglo[self.size-1] = None
+            self.size-1
+            return datoTemp
+    
+    #Devuelve el elemento en el tope de la pila
+    def tope(self):
+        if self.size == 0:
+            return None
+        else:
+            return self.arreglo[self.size-1]
+
+    #devuelve si la pila esta vacia
+    def vacia():
+        return self.size == 0
+
         
             
         
