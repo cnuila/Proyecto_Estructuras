191ac0ddfe141611fc69120a5b3ef6f912188887
Carlos Nuila <cnuia14@icloud.com>
2019-08-20 04:21:06 -0600

    Agregando main

---
+++ main.py
@@ -0,0 +1,17 @@
+
+def main():
+    menuPrincipal = 0
+    while menuPrincipal != 4:
+        print("1. Trabajar con Listas","2. Trabajar con Pias",
+             "3. Trabajar con Colas","4. Salir",sep='\n')
+        menuPrincipal = int(input("Ingrese su opcion: "))
+
+
+    
+
+
+
+
+
+
+main()
