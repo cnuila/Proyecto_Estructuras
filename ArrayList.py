9f23fa9025b7d22d18b2ac07ce48c8465538ddd4
stephmartinezh <stephanie_martinezh@hotmail.com>
2019-08-21 18:56:43 -0600

    Metodos arreglados

--- ArrayList.py
+++ ArrayList.py
@@ -4,14 +4,13 @@ class ArrayList(Lista):
 
     #constructor
     def __init__(self, cuenta, nombre):
-        Alumno.__init__(self, cuenta, nombre)
         self.capacidad = 0
         self.size = 0
         self.arreglo = []
 
     #verifica si la lista está vacía
     def vacia(self):
-            return self.size == 0
+        return self.size == 0
 
     #borra los elementos de la lista
     def anula(self):
@@ -31,14 +30,17 @@ class ArrayList(Lista):
             self.capacidad = newCapacity
 
     #corre los elementos del arreglo
-    #def corrimiento(final):
+    def corrimiento(final):
+        for i in range(size-1,0,-1):
+            arreglo[i+1] = arreglo[i]
+
         
     #agrega un elemento a la lista
     def inserta(self, dato, posicion):
         if posicion<= self.size+1 and posicion >= 1:
             self.ampliar()
             if posicion != self.size+1:
-                #self.corrimiento()
+                self.corrimiento()
             arreglo[posicion] = dato
             self.size+1
             return True
@@ -68,7 +70,7 @@ class ArrayList(Lista):
             if posicion == 1:
                 return None
             else:
-                return arreglo[pos-1]
+                return arreglo[posicion-1]
         else:
             return None
 
