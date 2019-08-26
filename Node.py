b9c02df829359e07f36991797cf60d83ffd0b326
Carlos Nuila <cnuia14@icloud.com>
2019-08-25 19:04:44 -0600

    Actualizando

--- Node.py
+++ Node.py
@@ -1,26 +1,26 @@
-from Object import Object
-from Pila import Pila
 
 class Node:
 
     #constructor
     def __init__(self):
+        self.anterior = Node()
+        self.siguiente = Node()
         self.anterior = None
         self.siguiente = None
         self.data = None
 
     #setters and getters
-    def setSiguiente(next):
+    def setSiguiente(self,next):
         self.siguiente = next
-    def getSiguiente():
+    def getSiguiente(self):
         return self.siguiente
 
-    def setAnterior(prev):
+    def setAnterior(self,prev):
         self.anterior = prev
-    def getAnterior():
+    def getAnterior(self):
         return self.anterior
 
-    def setData(pData):
-        self.data = pData
+    def setData(Data):
+        self.data = Data
     def getData():
-        return self.pData
+        return self.data
