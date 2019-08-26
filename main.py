cd21866a3b726789593153e28769eb8a258dfbc8
Daniel Rodriguez <josedanielrc@unitec.edu>
2019-08-26 08:03:54 -0600

    Main cola

--- main.py
+++ main.py
@@ -360,11 +360,11 @@ def main():
                         if operacionCola == 1:
                             if opcionCola == 1:
                                 #insetar codigo con arrayqueue
-                                data=input("Ingrese el Alumno(nombre-cuenta) a encolar")
+                                data=input("Ingrese el Alumno(nombre-cuenta) a encolar: ")
                                 arrayqueue.queue(data)
                             if opcionCola == 2:
                                 #insertar codigo con linkedqueue
-                                data=input("Ingrese el Alumno(nombre-cuenta) que desea en el nuevo nodo")
+                                data=input("Ingrese el Alumno(nombre-cuenta) que desea en el nuevo nodo: ")
                                 linkedqueue.queue(data)
                         elif operacionCola == 2:
                             if opcionCola == 1:
@@ -383,10 +383,10 @@ def main():
                         elif operacionCola == 4:
                             if opcionCola == 1:
                                 #insetar codigo con arrayqueue
-                                arrayqueue.isEmpty()
+                                print(arrayqueue.isEmpty())
                             if opcionCola == 2:
                                 #insertar codigo con linkedqueue
-                                linkedqueue.isEmpty()
+                                print(linkedqueue.isEmpty())
                         elif operacionCola == 5:
                             if opcionCola == 1:
                                 #insetar codigo con arrayqueue
