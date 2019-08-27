5455d68c7bcd9f0dab404282ffe7982bbfd66c24
Daniel Agurcia <danielagurcia90@gmail.com>
2019-08-27 06:16:51 -0600

    Actualizacion

--- LinkedList.py
+++ LinkedList.py
@@ -8,26 +8,112 @@ class LinkedList(Lista):
         self.inicio = None
         self.size = 0
     
+    #Agrega elemento a la lista en la posicion ingresada
     def inserta(self, dato, posicion):
         newNode = Node()
         newNode.setData(dato)
-        temp = self.inicio
-        if posicion > 1 and posicion < self.size:
-            for i in range(2,posicion-1,1):
-                temp = temp.getSiguiente()
-            temp.getSiguiente().se
-    
+        tmp = self.inicio
+        if self.inicio == None:
+            self.inicio = newNode
+            self.size = self.size + 1
+            return True
+        elif posicion == 1:
+            tmp.setAnterior(newNode)
+            newNode.setSiguiente(tmp)
+            self.size = self.size + 1
+            return True
+        elif posicion>1 and posicion<self.size:
+            for _ in range(2,posicion-1,1):
+                tmp = tmp.getSiguiente()
+            tmp.getSiguiente().setAnterior(newNode)
+            newNode.setSiguiente(tmp.getSiguiente())
+            newNode.setAnterior(tmp)
+            tmp.setSiguiente(newNode)
+            self.size = self.size + 1
+            return True
+        elif posicion==self.size:
+            for _ in range(posicion):
+                tmp = tmp.getSiguiente()
+            tmp.setSiguiente(newNode)
+            newNode.setAnterior(tmp)
+            self.size = self.size + 1
+            return True
+        else:
+            return False
+
+    #Elimina el elemento en la posicion ingresada
+    def borrarElemento(self, posicion):
+        if posicion==1:
+            if self.size>1:
+                tmp=self.inicio.getSiguiente()
+                tmp.getAnterior().setSiguiente(None)
+                tmp.setAnterior(None)
+                self.inicio=tmp
+            else:
+                self.inicio = None
+            self.size = self.size -1
+        elif posicion>1 and posicion<self.size:
+            tmp=self.inicio
+            for _ in range(posicion):
+                tmp = tmp.getSiguiente()
+            tmp.getSiguiente().setAnterior(tmp.getAnterior())
+            tmp.getAnterior().setSiguiente(tmp.getSiguiente())
+            tmp.setSiguiente(None)
+            tmp.setAnterior(None)
+            self.size = self.size -1
+        elif posicion == self.size:
+            for _ in range(self.size):
+                tmp = tmp.getSiguiente()
+            tmp.getAnterior().setSiguiente(None)
+            tmp.setAnterior(None)
+            self.size = self.size -1
+        else:
+            return None
+    #Devuelve la posicion del dato a buscar
     def buscar(self, dato):
-        pass
-
+        tmp = self.inicio
+        for i in range(self.size):
+            if (tmp.getData() is dato):
+                return i
+            else:
+                tmp = tmp.getSiguiente()
+    #Verifica si la lista esta vacia
     def vacia(self):
-        return self.inicio == None
-
+        if self.inicio == None:
+            return True
+        else:
+            return False
+    #Devuelve el dato en la posicion mandada
     def elementoPosicion(self, posicion):
         if posicion>=1 and posicion<=self.size:
             tmp = self.inicio
-            for i in range(self.size):
+            for _ in range(2,posicion):
                 tmp = tmp.getSiguiente()
-            return tmp.getData()
+            print("El dato del elemento en la posicion ",posicion, "es " ,tmp.getData())
         else:
-            return None
+            print("Ingreso una posicion incorrecta.")
+    #Devuelve el dato siguiente a la posicion ingresada
+    def obtenerSiguiente(self, posicion):
+        if posicion>=1 and posicion<self.size:
+            tmp = self.inicio
+            for _ in range(2,posicion):
+                tmp = tmp.getSiguiente()
+            print("El dato del elemento siguiente a la posicion ",posicion, "es " ,tmp.getData())
+        else:
+            print("Ingreso una posicion incorrecta.")
+    #Devuelve el dato anterior a la posicion ingresada
+    def obtenerAnterior(self, posicion):
+        if posicion>1 and posicion<=self.size:
+            tmp = self.inicio
+            for _ in range(2,posicion):
+                tmp = tmp.getSiguiente()
+            print("El dato del elemento anterior a la posicion ",posicion, "es " ,tmp.getData())
+        else:
+            print("Ingreso una posicion incorrecta.")
+    #Vacia la lista
+    def anula(self):
+        if self.inicio!= None:
+            self.inicio=None
+            print("La lista se vacio correctamente.")
+        else:
+            print("La lista esta vacia.")
